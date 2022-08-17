d67972c25538d4e51f2d3b91e4cf6399b1fe5b4a
leonardo-terra <leo.terra.melo@gmail.com>
2022-08-17 15:27:18 -0300

    Aula 1 - Iniciando projetos de outros tipos

---
+++ greet/greet.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace greet.Test;
+using FluentAssertions;
+public class UnitTest1
+{
+  [Theory(DisplayName = "Deve retornar 'Olá'")]
+  [InlineData("Olá")]
+  public void TestGreet(string expected)
+  {
+    var result = Class1.Greet();
+    result.Should().Be(expected);
+  }
+}
