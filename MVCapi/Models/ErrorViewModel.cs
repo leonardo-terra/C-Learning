4749ef6f4ca823270317b721cfea03da85af5053
leonardo-terra <leo.terra.melo@gmail.com>
2022-08-17 15:28:58 -0300

    Configurando main

--- MVCapi/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MVCapi.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- greet/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace greet;
-
-public class Class1
-{
-  public static string Greet()
-  {
-    return "Olá";
-  }
-}
